77935bcc9e9872f3cf94ade4eca0979d35fcfa0e
Mint Raines <git@mintyfresh.love>
2026-05-31 18:58:56 +1000

    First tile map + collision

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
+@export var SPEED = 100.0
+@export var JUMP_VELOCITY = -1000.0
+@export var GRAVITY = 200.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,8 +12,8 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,3 +24,20 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	player_animations()
+	
+func player_animations():
+	if Input.is_action_pressed("ui_left") || Input.is_action_just_released("ui_jump"):
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("Run")
+	if Input.is_action_pressed("ui_right") || Input.is_action_just_released("ui_jump"):
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("Run")
+	if !Input.is_anything_pressed():
+		$AnimatedSprite2D.play("Idle")
+
+func _input(event):
+	# jumping
+	if event.is_action_pressed("ui_jump") and is_on_floor():
+		velocity.y += JUMP_VELOCITY
+		$AnimatedSprite2D.play("Jump")
